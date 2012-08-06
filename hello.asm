ace52b95f4bd2a691b9670a69803da58a9fe692e
Smit Mehta <smit.meh@gmail.com>
2012-08-06 14:40:05 +0530

    pdf reference

--- hello.asm
+++ hello.asm
@@ -1,18 +1,30 @@
 section .data
-	hello:     db 'Hello world!',10    ; 'Hello world!' plus a linefeed character
-	helloLen:  equ $-hello             ; Length of the 'Hello world!' string
-	                                   ; (I'll explain soon)
+hello:     db 'Hello world!',10    ; 'Hello world!' plus a linefeed character
+helloLen:  equ $-hello             ; Length of the 'Hello world!' string
+array:     db 1, 2, 3, 4
+ans dw 0
 
 section .text
-	global _start
+global _start
 
 _start:
-	mov eax,4            ; The system call for write (sys_write)
-	mov ebx,1            ; File descriptor 1 - standard output
-	mov ecx,hello        ; Put the offset of hello in ecx
-	mov edx,helloLen     ; helloLen is a constant, so we don't need to say
-	                     ;  mov edx,[helloLen] to get it's actual value
-	int 80h              ; Call the kernel
+	mov ax, 0
+	mov bl, 0
+	mov ch, 0
+	loopstart:
+		mov cx,array[1]
+		add ax, cx
+		inc bl
+		cmp bl, 4
+		jl loopstart
+
+	add ax, 48
+	mov [ans] , ax
+	mov ecx ,ans
+	mov eax, 4
+	mov ebx, 1
+	mov edx,1 
+	int 80h
 
 	mov eax,1            ; The system call for exit (sys_exit)
 	mov ebx,0            ; Exit with return code of 0 (no error)
